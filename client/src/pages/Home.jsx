cffa84f1bc8b7547e619e191751f2693982ca15b
maranathaadesanya <adesanyamaranatha@yahoo.com>
2023-10-05 11:03:38 +0100

    UPDATING

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+   // eslint-disable-next-line
+import { ToastContainer, toast } from "react-toastify";
+
+const Home = () => {
+  const navigate = useNavigate();
+     // eslint-disable-next-line
+  const [cookies, removeCookie] = useCookies([]);
+     // eslint-disable-next-line
+  const [username, setUsername] = useState("");
+
+  const Login = () => {
+    removeCookie("token");
+    navigate("/signup");
+  };
+  return (
+    <>
+      <div className="home_page">
+        <h4>
+          {" "}
+          TASKTRACK
+        </h4>
+        <button onClick={Login}>WELCOME</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default Home;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,120 @@
+// eslint-disable-next-line
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
+import { useCookies } from "react-cookie";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputValue;
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-left",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/login",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+      console.log(data);
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 200);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      email: "",
+      password: "",
+    });
+  };
+
+  const [cookies, removeCookie] = useCookies([]);
+  const [ username, setUsername] = useState("");
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      const { data } = await axios.post(
+        "http://localhost:4000",
+        {},
+        { withCredentials: true }
+      );
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
+  return (
+    <div className="form_container">
+      <h2>Login Account <span>{username}</span></h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={handleOnChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <span>
+          Already have an account? <Link to={"/signup"}>Signup</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Login;
